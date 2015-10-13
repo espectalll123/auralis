3df0526b6d6dbeee8cb4fb70d62f1fe6f7fcb61d
Francisco Gómez <espectalll123@kydara.com>
2015-10-13 22:39:06 +0200

    I'm dying inside...

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use std::env;
+extern crate hyper;
+extern crate rustc_serialize;
 
-fn main() {
-    let mut count = 0;
-    
+// use std::env;
+use std::io::Read;
+
+use hyper::Client;
+use rustc_serialize::json::Json;
+
+fn main() {    
     println!("When I grow up, I wanna be an AUR helper!");
+    
+    /*
     println!("Meanwhile, here are your arguments <3");
+    let mut count = 0;
     for argument in env::args() {
         count += 1;
         println!("{}· {}", count, argument);
     }
+     */
+
+    // Should implement a let for the RPC URL...
+    let mut client = Client::new();
+    
+    let mut res = client.get("https://aur.archlinux.org/rpc.php?type=search&arg=cowsay")
+        .send().unwrap();
+    let mut res_body = String::new();
+    res.read_to_string(&mut res_body)
+        .unwrap();
+    let json_data = Json::from_str(&res_body)
+        .unwrap();
+    let json_obj = json_data
+        .as_object()
+        .unwrap();
+    let results = json_obj.get("results")
+        .unwrap();
+    // I *really* have no idea about Rust...
 }
