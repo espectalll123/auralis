e8a406ddaa6e65ce83e09d9e8b981c5b55d7d22d
Francisco Gómez <espectalll123@kydara.com>
2015-10-13 18:51:53 +0200

    Trying to do something :new_moon_with_face:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
+    let mut count = 0;
+    
     println!("When I grow up, I wanna be an AUR helper!");
+    println!("Meanwhile, here are your arguments <3");
+    for argument in env::args() {
+        count += 1;
+        println!("{}· {}", count, argument);
+    }
 }
