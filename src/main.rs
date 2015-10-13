6b2c0a7499d4f37e4c054c18aca954977b1dd144
Francisco Gómez <espectalll123@kydara.com>
2015-10-13 23:32:27 +0200

    Byotiful

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,21 @@ use rustc_serialize::json::Json;
 
 fn main() {    
     println!("When I grow up, I wanna be an AUR helper!");
-    
-    /*
-    println!("Meanwhile, here are your arguments <3");
-    let mut count = 0;
-    for argument in env::args() {
-        count += 1;
-        println!("{}· {}", count, argument);
-    }
-     */
-
     // Should implement a let for the RPC URL...
     let mut client = Client::new();
     
     let mut res = client.get("https://aur.archlinux.org/rpc.php?type=search&arg=cowsay")
         .send().unwrap();
     let mut res_body = String::new();
-    res.read_to_string(&mut res_body)
-        .unwrap();
-    let json_data = Json::from_str(&res_body)
-        .unwrap();
-    println!("{}", json_data.pretty());
-    /*
-    let json_obj = json_data
-        .as_object()
-        .unwrap();
-    let results = json_obj.get("results")
-        .unwrap();
-     */
-    
-    // I *really* have no idea about Rust...
+    res.read_to_string(&mut res_body).unwrap();
+    let json_data = Json::from_str(&res_body).unwrap();
+    let results = json_data.as_object().unwrap().get("results").unwrap();
+
+    for app in results.as_array().unwrap() {
+        print!("\n");
+        println!("Name: {}", app.as_object().unwrap().get("Name").unwrap());
+        println!("Version: {}", app.as_object().unwrap().get("Version").unwrap());
+        println!("Description: {}", app.as_object().unwrap().get("Description").unwrap());
+        println!("Nº of votes: {}", app.as_object().unwrap().get("NumVotes").unwrap());
+    }
 }
